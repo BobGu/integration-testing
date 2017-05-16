0e57f6e420dd80d3b93fbe72a18b47f7693c6cb5
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2017-05-16 07:46:34 -0700

    Add setup for integration tests

---
+++ test/unit/index.js
@@ -0,0 +1,28 @@
+var assert = require('chai').assert;
+var request = require('request');
+
+describe('Fake Unit Tests', function(){
+  it('True is equal to true', function(){
+    assert.equal(true, true);
+  });
+
+  it('Earth is flat', function(){
+    assert.equal(true, true);
+  });
+
+  it('Turing is cool', function(){
+    assert.equal(true, true);
+  });
+
+  it('Pizza is yummy', function(){
+    assert.equal(true, true);
+  });
+
+  it('Mean people are mean', function(){
+    assert.equal(true, true);
+  });
+
+  it('Spinners gonna spin', function(){
+    assert.equal(true, true);
+  });
+});
